e98fac1e994b340443e6aa48783e36830a3290f6
Lucas Previato <lucaspreviato@Live.com>
2024-02-10 02:26:40 -0300

    feat(Sidebar): complete sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -1,19 +1,27 @@
 <template>
     <v-navigation-drawer v-model="drawer" color="#2B2B2B">
       <div class="pa-4">
-          <v-img src="/p1.webp" width="150" height="150px" class="ml-9"></v-img>
+          <v-img src="/sales-ui-logo-m2.png" width="150" height="150px" class="ml-9"></v-img>
       </div>
       <v-card  class="rounded-e-xl me" width="60" height="400" color="#545454"></v-card>
       <v-list class="top">
-          <v-list-item v-for="(item, i) in navegationLinks" :key="i" :value="item" class="mb-5" color="#FF6259" variant="plain">
+          <v-list-item v-for="(item, i) in navegationLinks" :key="i" :value="item" class="mb-5" color="#90EE90" variant="plain">
               <template v-slot:append="{isActive}">
                <div v-if="isActive" class="a"></div>
               </template>
               <template v-slot:prepend>
                   <v-icon :icon="item.icon"></v-icon>
               </template>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
           </v-list-item>
       </v-list>
+      <v-div style="position: absolute; bottom: 20px; margin-left: auto; margin-right: auto; left: 0; right: 0; text-align: center;">
+        <v-btn icon="mdi mdi-cog" size="small" class="mr-2" color="#5F3B39"></v-btn>
+      <v-badge dot color="#FF6259">
+        <v-btn icon="mdi mdi-bell" size="small" color="#5F3B39"></v-btn>
+      </v-badge>
+      </v-div>
+     
     </v-navigation-drawer>
   </template>
   
@@ -37,8 +45,6 @@
       }
   }
   </script>
-  
- 
 
 <style scoped>
 
@@ -65,9 +71,7 @@
     position: absolute;
     height: 15px;
     width: 15px;
-    background: linear-gradient(-90deg,#545454,#545454 50%,black 50%);
+    background: linear-gradient(-90deg,#90EE90,#90EE90 50%,black 50%);
     border-radius: 50%;
-
   }
-
 </style>
